380e93a896a5139b27af7a5f015263fd8f16e6fc
Sang Jun Kim <sangjunkim@Sangjun-Kimui-M1-PRO.local>
2022-10-11 15:41:12 +0900

    메인 페이지 html 수정

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'etc/secret_key.json')  # secrets.json 파일 위치를 명시
+secret_file = os.path.join(BASE_DIR, 'secret_key.json')  # secrets.json 파일 위치를 명시
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
@@ -39,6 +39,8 @@ def get_secret(setting):
 SECRET_KEY = get_secret("SECRET_KEY")    
 
 # Application definition
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -95,8 +97,8 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'ROHDE&SCHWARZ4',
-        'USER':'postgres',
+        'NAME':'ROHDE&SCHWARZ3',
+        'USER':'sangjunkim',
         'PASSWORD': '1234qwer',
         'HOST': 'localhost',
         'PORT':'5432',
--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['3.37.231.229']
+ALLOWED_HOSTS = ['*']
